3f20a53a1c6f81ea705ad247ab7b0f1d424a11f7
SierraGoldNurseries <gurmail@sgtrees.com>
2026-04-25 22:02:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const SG_LOGO_URL = "https://11150895.app.netsuite.com/core/media/media.nl?id=15
 const GENEVA_SG_LOGO_URL = "https://11150895.app.netsuite.com/core/media/media.nl?id=260263&c=11150895&h=NMkHvroppy8Yi93204J1rZiq_7V-dJBmcFNuScfEc2hRzqB9";
 const GENEVA_LOGO_SHIFT_Y = -1;
 const OUTER_CARD_EXTRA_WIDTH = 260;
-const DEBUG_LAYER_LABELS_DEFAULT = true;
+const DEBUG_LAYER_LABELS_DEFAULT = false;
 
 /*
   v8.6.8 config:
@@ -24,7 +24,7 @@ const OUTER_CARD_SIZE_CONFIG = {
   applyTo: ["POT", "WRAP"]
 };
 const LAYER_DEBUG_CONFIG = {
-  enabled: false,
+  enabled: true,
   movable: true,
   rememberPosition: true,
   defaultLeft: 18,
